448c127aa9a15d1dbc42d4cb734a83ab16c608db
RJ <rongjihuang@gmail.com>
2018-11-26 23:36:44 +0800

    Taskbar

---
+++ src/components/task.vue
@@ -0,0 +1,54 @@
+<template>
+<div class="st-task"
+  :class="hovering ? hoverClass : ''"
+  :title="label"
+  @mouse-hover="hovering = true"
+  @mouse-out="hovering = false">
+  <i class="st-task-icon" :class="icon"></i>
+  <label class="st-task-label">{{label}}</label>
+</div>
+</template>
+
+<script>
+export default {
+  props: {
+    label: { type: String, require: false },
+    icon: { type: String, require: false, default: "st-task-default" },
+    value: { require: false },
+    hoverable: { type: Boolean, require: false, default: true },
+    hoverClass: { type: String, require: false, default: "st-state-hover" }
+  },
+  data() {
+    return { hovering: false };
+  }
+};
+</script>
+
+<style>
+.st-task {
+  display: inline-flex;
+  flex-direction: row;
+  margin-right: 0.25em;
+  max-width: 5em;
+  border: 1px solid transparent;
+  background-color: #222;
+}
+.st-task-icon {
+  font-style: normal;
+  width: 1em;
+}
+.st-task-label {
+  flex-grow: 1;
+  min-width: 2em;
+  padding: 0.25em;
+  background-color: #222;
+  box-sizing: border-box;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+}
+.st-task:hover {
+  border: 1px solid gold;
+  cursor: pointer;
+}
+</style>
--- src/index.vue
+++ src/index.vue
@@ -12,12 +12,24 @@
     </div>
     <div class="sidebar">Sidebar</div>
   </div>
-  <div class="footer">©2017 simter-vue-desktop</div>
+  <div class="footer">
+    <div class="copyright">©2017 simter-vue-desktop</div>
+    <div class="toggler"></div>
+    <div class="tasks-bar">
+      <st-task
+        v-for="(s, index) of tasks"
+        :key="index"
+        :label="s.label"
+        :icon="s.icon">
+      </st-task>
+    </div>
+  </div>
 </div>
 </template>
 
 <script>
 import stShortcut from "./components/shortcut.vue";
+import stTask from "./components/task.vue";
 
 export default {
   data() {
@@ -28,10 +40,16 @@ export default {
         for (let i = 0; i < n; i++)
           ss.push({ label: (i < 5 ? `User ${i + 1}` : `User ${i + 1} abc def`), icon: "st-icon-user" });
         return ss;
-      })(35)
+      })(35),
+      tasks: (n => {
+        const ss = [];
+        for (let i = 0; i < n; i++)
+          ss.push({ label: (i < 5 ? `User ${i + 1}` : `User ${i + 1} abc def`), icon: "st-icon-user" });
+        return ss;
+      })(6)
     };
   },
-  components: { stShortcut }
+  components: { stShortcut, stTask }
 };
 </script>
 
@@ -42,29 +60,55 @@ export default {
   flex-direction: column;
 }
 .st-layout > .header {
+  position: relative;
   background-color: #111;
   text-align: center;
   padding: 0.25em;
+  border-bottom: 1px solid #222;
 }
 .st-layout > .footer {
+  position: relative;
   background-color: #111;
+  min-height: 2.5em;
+  border-top: 1px solid #222;
+}
+.st-layout > .footer > .copyright {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
   text-align: center;
   padding: 0.25em;
   color: #888;
 }
+.st-layout > .footer > .toggler {
+  position: absolute;
+  border-left: 1px solid #222;
+  bottom: 0;
+  top: 0;
+  right: 0;
+  width: 10px;
+}
+.st-layout > .footer > .tasks-bar {
+  padding: 0.25em;
+}
+
 .st-layout > .middle {
   flex-grow: 1;
   display: flex;
   flex-direction: row;
 }
 .st-layout > .middle > .sidebar {
+  position: relative;
   min-width: 20em;
   background-color: #111;
 }
 .st-layout > .middle > .content {
+  position: relative;
   flex-grow: 1;
   overflow: auto;
   padding-right: .5em;
   padding-bottom: .5em;
+  border-right: 1px solid #222;
 }
 </style>
